98145b8803b7f5ac17b83c3970b1d978ca87e54c
swaroop0202 <swaroopk372@gmail.com>
2024-04-20 15:21:35 +0530

    commitiing

---
+++ 05-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("raki" "badshah" "alluduseenu")
+
+echo"name of the first movie is ${MOVIE[0]}"
+echo"name of the first movie is ${MOVIE[1]}"
+echo"name of the first movie is ${MOVIE[2]}"
+echo"name of the first movie is ${MOVIE[@]}"
