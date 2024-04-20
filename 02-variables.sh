a5dde1e06a06b8ba507446c046843c2fa512f103
swaroop0202 <swaroopk372@gmail.com>
2024-04-20 14:45:39 +0530

    commitiing

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1: hi $PERSON2 how r u?"
+echo "$PERSON2: hi $PERSON1 am fine hw about u?"
+echo "$PERSON1: am fine..thinking to learn devops"
