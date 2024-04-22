d5ec52619159f97347fd0d2bafa8cbdc18717abc
swaroop0202 <swaroopk372@gmail.com>
2024-04-22 18:05:48 +0530

    commitiing

---
+++ 08-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+  echo "$2....failure"
+  exit1
+  else
+  echo "$2...success"
+
+}
+
+if [ $USERID -ne 0 ]
+  then
+       echo "you are not a super user"
+  else
+       echo "you are a super user"
+fi          
+dnf install mysql -y
+
+VALIDATE $? "installing mysql"
